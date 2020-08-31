8cf946a6c75824c609ae7b32bbf84c639a9c5bc2
root <root@instance-20200831-1111.subnet.vcn.oraclevcn.com>
2020-08-31 09:06:41 +0000

    Deploy website - based on

---
+++ 90ae50ee.ac9930a1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{68:function(o){o.exports=JSON.parse('{"allTagsPath":"/test-site/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/test-site/blog/tags/facebook"}')}}]);
---
+++ 92baba8f.85fe0e60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{69:function(a){a.exports=JSON.parse('{"allTagsPath":"/test-site/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/test-site/blog/tags/hola"}')}}]);
